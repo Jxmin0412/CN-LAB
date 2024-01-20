07922b593b90a9ff51b7f652bec96b32031e752e
Rishav Bhagat <rishavsci@gmail.com>
2024-01-20 11:26:02 +0530

    Create var_len.c

---
+++ var_len.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+int i=0,j,s,c=0;
+char msg[100];
+printf("Enter string: ");
+scanf("%s",msg);
+while(msg[i]!='\0')
+{
+printf("enter frame size of %d: ",c);
+scanf("%d",&s);
+printf("message of %d frame is: ",c++);
+for(j=i;j<i+s;j++)
+{
+printf("%c",msg[j]);
+}
+i+=s;
+printf("\n");
+}
+}
