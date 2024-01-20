dcd9129886dd411e80ca9a2eeefe526809623949
Rishav Bhagat <rishavsci@gmail.com>
2024-01-20 11:24:53 +0530

    Create fixed_len.c

---
+++ fixed_len.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+void main()
+{
+int i,j,l,s,c=0;
+char str[25],msg[25];
+printf("enter string: ");
+scanf("%s",&str);
+printf("enter frame size: ");
+scanf("%d",&s);
+l=0,j=0;
+msg[0]='\0';
+for(i=0;str[i]!='\0';i++)
+{
+if(l==s)
+{
+printf("msg of frame %d is: %s\n",c++,msg);
+l=0,j=0;
+}
+l++;
+msg[j]=str[i];
+j++;
+msg[j]='\0';
+}
+printf("msg of frame %d is: %s",c++,msg);
+}
